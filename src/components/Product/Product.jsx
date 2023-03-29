1de7742ac72a453b429986bce018d30db4574c49
sayma-akther <saymaakther126@gmail.com>
2023-03-29 23:40:01 +0600

    next part added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FaCartPlus } from 'react-icons/fa';
 import './Product.css';
 
 const Product = (props) => {
   const {img,name,price,seller,ratings} = props.product;
+  const handleAddToCart = props.handleAddToCart;
+
   return (
     <div className='product'>
       <img src={img} alt="image" />
@@ -12,7 +15,7 @@ const Product = (props) => {
         <p>Manufecturar: {seller}</p>
         <p>Ratings: {ratings} Stars</p>
       </div>
-      <button className='btn-cart'>Add to cart</button>
+      <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to cart <FaCartPlus /></button>
     </div>
   );
 };
