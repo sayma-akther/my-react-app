7c0271e0fe9eed8f7e656be20cb15e73648c7ef3
sayma-akther <saymaakther126@gmail.com>
2023-03-30 16:28:52 +0600

    retraive product from stored cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -8,15 +9,37 @@ const Shop = () => {
 
   const [cart, setCart] = useState([])
 
-  useEffect( () =>{
+  useEffect(() => {
     fetch('products.json')
-    .then(res => res.json())
-    .then(data => setProducts(data))
+      .then(res => res.json())
+      .then(data => setProducts(data))
   }, []);
+
+  useEffect(() => {
+    const storedCart = getShoppingCart()
+    const saveCart = [];
+    //step-1: get id
+    for (const id in storedCart) {
+      //step-2: get the product by using id
+      const addedProduct = products.find(product => product.id === id);
+      if (addedProduct) {
+        //step-3:get quantity of the product
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        //step-4: added the product to the save cart
+        saveCart.push(addedProduct);
+      }
+      console.log(addedProduct);
+    }
+    //step-5:set the cart
+    setCart(saveCart);
+
+  }, [products]);
 
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id)
   }
 
   return (
@@ -24,14 +47,14 @@ const Shop = () => {
       <div className="product-container">
         {
           products.map(product => <Product
-           key={product.id}
-           product={product}
-           handleAddToCart = {handleAddToCart}
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
           ></Product>)
         }
       </div>
       <div className="cart-container">
-        <Cart cart = {cart}></Cart>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
